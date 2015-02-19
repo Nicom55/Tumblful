d3b71a8e6b215ef0bf898c693f00ff53fcdb4f45
Nico Moreno <nicom55@gmail.com>
2015-02-19 12:45:20 +0000

    added jQuery to increment likes with AJAX

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,15 @@
 class LikesController < ApplicationController
 
   def create
+    @resource = find_resource
     @like = Like.create(like_params)
     redirect_to :back
+    
+    # Respond to different request formats (like AJAX)
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
   end
 
   private
@@ -11,4 +18,10 @@ class LikesController < ApplicationController
     params.require(:like).permit(:user_id, :likeable_type, :likeable_id)
   end
   
+  def find_resource
+    type = params[:like][:likeable_type].constantize
+    id = params[:like][:likeable_id]
+
+    type.send(:find, id)
+  end
 end
